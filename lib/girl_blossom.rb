4d2569fecc62462054638ff2c7803bc1406a9be8
Dani Bassett <dani@danithetallgirl.com>
2020-07-28 17:52:06 +0000

    add description

---
+++ lib/girl_blossom.rb
@@ -0,0 +1,6 @@
+require "girl_blossom/version"
+
+module GirlBlossom
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/girl_blossom/version.rb
@@ -0,0 +1,3 @@
+module GirlBlossom
+  VERSION = "0.1.0"
+end
