97668b3a940390a84e77eae32469ec9e9ebbe23b
danithetallgirl <dani@danithetallgirl.com>
2020-07-28 23:15:21 -0400

    Done.

--- lib/girl_blossom/normal.rb
+++ lib/girl_blossom/normal.rb
@@ -1,9 +1,18 @@
-require_relative "./girl_blossom/dry.rb"
-class GirlBlossom::Normal < GirlBlossom::Dry
+class GirlBlossom::Normal
+    @@all = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        save
+    end
 
     def self.all
         GirlBlossom::Scraper.scrape_normal if @@all.empty?
         @@all
     end
 
+    def save
+        @@all << self
+    end
 end
